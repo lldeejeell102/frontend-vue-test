fdb4a6c23341d5c8c2ce1c2872108b46e063149f
Daniel Ji <daniel.ji0923@gmail.com>
2024-04-11 11:16:27 -0700

    updated main page to be simple

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
       <router-link to="/people">People</router-link>
     </nav>
     <router-view/>
